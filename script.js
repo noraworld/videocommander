39029d4480afb368d1b559d0c6d1304fdf4d1de8
Kosuke Aoki <mail@noraworld.com>
2022-08-03 03:23:21 +0900

    Retrieve video element automatically when moving to another page
    The feature 2c48bf3ec15b59f2587b41cfc3e743f77a40e2a4 was deleted because
    I'm not sure what it was and where the problem occurred

--- script.js
+++ script.js
@@ -65,7 +65,6 @@ $(function() {
   var loopFlag = false;
   var loopTimeoutID;
   var removeStatusBoxTimeoutID;
-  var getVideoTimeoutID;
   var setPlaybackSpeedSuccessfullyOrSkipable = false;
   var videoWidth;
   var videoHeight;
@@ -78,7 +77,7 @@ $(function() {
   var isSpeedChangedFromThisExtension = false;
 
   // video要素を取得する
-  function getVideoElement(signal) {
+  function getVideoElement(signal = '') {
     if (document.getElementsByTagName(playerType)[playerOrder] !== undefined) {
       if (document.getElementsByTagName(playerType)[playerOrder].readyState === 4) {
         player = document.getElementsByTagName(playerType)[playerOrder];
@@ -90,14 +89,6 @@ $(function() {
         }
         else {
           enableNotFullscreenProgressBar();
-        }
-
-        try {
-          clearTimeout(getVideoTimeoutID);
-        }
-        catch (err) {
-          // Uncomment when debugging.
-          // console.warn('Failed to clear getVideoTimeoutID. But continuing.');
         }
 
         observeSpeed();
@@ -123,16 +114,9 @@ $(function() {
       }
     }
 
-    if (player === undefined) {
-      getVideoTimeoutID = setTimeout(function() {
-        // Some video sites has several video elements which are invisible
-        // and are never ready ("readyState" is always "0").
-        // If the first video element (0th element) on that page is never ready,
-        // it cannot take the actual video element (visible video’s element),
-        // so it searches all video elements in order until the actual video element are found.
-        getNextVideoElement();
-      }, 200);
-    }
+    setTimeout(function() {
+      getVideoElement()
+    }, 1000)
   }
 
   // Shown when FULLSCREEN
